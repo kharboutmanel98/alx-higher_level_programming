eb750dd53f3d789214d46c9d232d7056bbc3a79f
ManelKharbout <111638160+kharboutmanel98@users.noreply.github.com>
2024-05-10 13:48:23 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/* Write a script that display the status code of a GET request. */
-
-const request = require('request');
-const url = process.argv[2];
-request.get(url).on('response', response => {
-  const status = `${response.statusCode}`;
-  console.log('code:', status);
-});
